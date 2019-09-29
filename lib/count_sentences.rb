2bb49782dcf863570eb465eef6a092d811e401d9
Stef Spears <spears.sj87@gmail.com>
2019-09-29 03:23:29 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     sentences = self.split(/[.!?]/)
-    sentences.reject { |i| i.empty? }
+    sentences.reject! { |i| i.empty? }
     sentences.count
   end
 end
